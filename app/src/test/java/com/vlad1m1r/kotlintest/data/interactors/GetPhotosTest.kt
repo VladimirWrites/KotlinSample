797684d5041a0c8b60be73816949a2dc961c66b0
Vladimir Jovanovic <v.jovanovic@holidaypirates.com>
2017-04-06 16:33:15 +0200

    Improved GetPhotosTest

--- app/src/test/java/com/vlad1m1r/kotlintest/data/interactors/GetPhotosTest.kt
+++ app/src/test/java/com/vlad1m1r/kotlintest/data/interactors/GetPhotosTest.kt
@@ -16,13 +16,38 @@ class GetPhotosTest {
 
         val photosData:ArrayList<PhotoData> = arrayListOf()
 
-        (0..9).mapTo(photosData) { PhotoData(100+ it, it, "test"+ it, "url"+ it, "thumbnail"+ it) }
+        (0..3).mapTo(photosData) {
+            PhotoData(
+                    albumId = 100 + it,
+                    id = it,
+                    title = "test" + it,
+                    url = "url" + it,
+                    thumbnailUrl = "thumbnail" + it
+            )
+        }
 
         val photos = getPhotos.formatData(photosData)
-        for(i:Int in 0..9) {
+        for(i:Int in 0..3) {
             val photo = photos[i]
             assertEquals(photo.name, "test"+i)
             assertEquals(photo.url, "url"+i)
+        }
+
+        val photosDataEmpty:ArrayList<PhotoData> = arrayListOf()
+
+        (0..3).mapTo(photosDataEmpty) {
+            PhotoData(
+                    albumId = 100 + it,
+                    id = it,
+                    thumbnailUrl = "thumbnail" + it
+            )
+        }
+
+        val photosEmpty = getPhotos.formatData(photosDataEmpty)
+        for(i:Int in 0..3) {
+            val photo = photosEmpty[i]
+            assertEquals(photo.name, "")
+            assertEquals(photo.url, "")
         }
     }
 }
