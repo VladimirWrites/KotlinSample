e4e26423fd557914ecda554a4414f6b4f7c71a0f
Vladimir Jovanovic <v.jovanovic@holidaypirates.com>
2017-04-07 16:33:07 +0200

    Empty onCreate method removed in the BaseActivity class

--- app/src/main/java/com/vlad1m1r/kotlintest/presentation/base/BaseActivity.kt
+++ app/src/main/java/com/vlad1m1r/kotlintest/presentation/base/BaseActivity.kt
@@ -1,6 +1,6 @@
 package com.vlad1m1r.kotlintest.presentation.base
 
-import android.os.Bundle
+
 import android.support.v7.app.AppCompatActivity
 
 
@@ -8,9 +8,4 @@ import android.support.v7.app.AppCompatActivity
  * Created by vladimirjovanovic on 3/17/17.
  */
 
-abstract class BaseActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-}
+abstract class BaseActivity : AppCompatActivity() 
