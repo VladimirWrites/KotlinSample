b664ce5ec4a9a8ef39a3fbbc2a80c56aeb9a7bd0
Vladimir Jovanovic <j0v4n0v1c@gmail.com>
2018-01-09 00:05:13 +0100

    Clean architecture Covered with Unit tests

--- app/src/main/java/com/vlad1m1r/kotlintest/data/ApiInterface.kt
+++ app/src/main/java/com/vlad1m1r/kotlintest/data/ApiInterface.kt
@@ -18,8 +18,6 @@ package com.vlad1m1r.kotlintest.data
 
 import com.vlad1m1r.kotlintest.data.models.PhotoData
 
-import java.util.ArrayList
-
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -27,6 +25,6 @@ import retrofit2.http.Query
 interface ApiInterface {
 
     @GET("photos")
-    fun getPhotos(@Query("_start") offset:Int, @Query("_limit") limit:Int): Observable<ArrayList<PhotoData>>
+    fun getPhotos(@Query("_start") offset: Int, @Query("_limit") limit: Int): Observable<ArrayList<PhotoData>>
 
 }
--- app/src/main/java/com/vlad1m1r/kotlintest/presentation/list/ListPresenter.kt
+++ app/src/main/java/com/vlad1m1r/kotlintest/presentation/list/ListPresenter.kt
@@ -16,29 +16,27 @@
 
 package com.vlad1m1r.kotlintest.presentation.list
 
-import android.support.annotation.StringRes
 import com.vlad1m1r.kotlintest.R
-import com.vlad1m1r.kotlintest.data.interactors.GetPhotos
-import com.vlad1m1r.kotlintest.data.models.ItemPhoto
-
-import java.util.ArrayList
-
+import com.vlad1m1r.kotlintest.domain.interactors.GetPhotos
+import com.vlad1m1r.kotlintest.domain.models.ItemPhoto
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.observers.DisposableSingleObserver
 import io.reactivex.schedulers.Schedulers
+import java.net.UnknownHostException
+import java.util.*
 
-class ListPresenter(private val view: ListContract.View, private val getPhotos: GetPhotos) : ListContract.Presenter {
+class ListPresenter(private val view: ListContract.View, private val getPhotos: GetPhotos, private val disposables: CompositeDisposable) : ListContract.Presenter {
 
     val LIMIT = 20
-
-    private val disposables = CompositeDisposable()
 
     init {
         this.view.setPresenter(this)
     }
 
-    override fun onStart() {}
+    override fun start() {
+        loadData(0)
+    }
 
     override fun onDestroy() {
         disposables.clear()
@@ -51,10 +49,9 @@ class ListPresenter(private val view: ListContract.View, private val getPhotos: 
                         .getPhotos(offset, LIMIT)
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
-                        .single(ArrayList<ItemPhoto>())
+                        .single(ArrayList())
                         .subscribeWith(
                                 object : DisposableSingleObserver<ArrayList<ItemPhoto>>() {
-
                                     override fun onSuccess(photos: ArrayList<ItemPhoto>) {
                                         if (photos.size > 0) {
                                             if (offset > 0) {
@@ -70,15 +67,15 @@ class ListPresenter(private val view: ListContract.View, private val getPhotos: 
                                     }
 
                                     override fun onError(e: Throwable) {
-                                        view.showError(R.string.error__request_error)
+                                        view.showList(ArrayList())
+                                        when (e) {
+                                            is UnknownHostException -> view.showError(R.string.error__check_internet_connection)
+                                            else -> view.showError(R.string.error__request_error)
+                                        }
                                     }
+
                                 }
                         )
         )
-    }
-
-    override fun loadingDataError(@StringRes error: Int) {
-        view.showList(ArrayList())
-        view.showError(error)
     }
 }
